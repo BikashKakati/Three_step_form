55c8c1719e7be6c5faa6bd54c8e43640df3b15a4
Bikash Kakati <bikashkakati796@gmail.com>
2024-11-07 22:06:19 +0530

    Fix: Header content responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 const Header = () => {
   return (
-    <header className="w-full py-6 flex items-center relative">
-      <p className="flex-1 mx-1 md:text-center text-2xl font-medium text-background">
+    <header className="w-full py-6 flex items-center relative px-2">
+      <p className="flex-1 mx-1 md:text-center text-xl md:text-2xl font-medium text-background">
         Create New Account
       </p>
-      <p className="absolute top-1/2 right-0 -translate-y-1/2 text-foreground">
+      <p className="absolute top-1/2 right-5 -translate-y-1/2 text-foreground">
         Contact us
       </p>
     </header>
